290f8f791f291322d452c768a61e4293cda91ddf
SelkoSays <selkomrakec@gmail.com>
2022-06-10 16:56:19 +0200

    Revert "Adding binary search"
    This reverts commit 07fd0718979e2a90c3a2874b270c4b664335293f.

--- DataStructures/DemoArrayADT/DemoArrayADT/main.cpp
+++ DataStructures/DemoArrayADT/DemoArrayADT/main.cpp
@@ -1,11 +1,10 @@
 #include "Array20.h"
 #include "Array10.h"
-#include "Array.h"
 
 int main()
 {
 	int tmp[]{ 1,2,3,4,5 };
-	Array arr( tmp, 10, 5 );
+	Array10 arr( tmp, 5 );
 	arr.display();
 	arr.append(10);
 	arr.insert(0, 90);
